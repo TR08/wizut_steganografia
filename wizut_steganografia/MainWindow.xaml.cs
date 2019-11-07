fef813475798a8e66eabcd1279983dc57c4e92e6
Jacek Wolny <jacekwolny8@wp.pl>
2019-11-07 14:33:44 +0100

    ukończone umieszczanie wiadomości, odczytywanie w trakcie

--- wizut_steganografia/MainWindow.xaml.cs
+++ wizut_steganografia/MainWindow.xaml.cs
@@ -40,12 +40,20 @@ namespace wizut_steganografia
         {
             if (_imageLoaded)
             {
+                _imageEncrypted = false;
+                SaveImg.Source = new BitmapImage(new Uri("/images/no-img.png", UriKind.Relative));
+
                 Bitmap ImgToMod = new Bitmap(LoadedImgPath.Content.ToString());
-                string MsgToPut = "test";
-                byte[] EncodedMsg = EncodeMsg(MsgToPut);
+                byte[] EncodedMsg = EncodeMsg(CustomMsg.Text);
                 ImgToMod = PutMsg(ImgToMod, EncodedMsg);
+                if (ImgToMod == null)
+                {
+                    UpdateStatus("Message is too long for this bitmap file.");
+                    return;
+                }
                 SaveImg.Source = Bitmap2BitmapSource(ImgToMod);
                 _imageEncrypted = true;
+                UpdateStatus("Message embedded into bitmap file.");
                 ImgToMod.Dispose();
             }
         }
@@ -81,51 +89,120 @@ namespace wizut_steganografia
             return encrypted;
         }
 
-        //private byte[] RefillWithZeros(int value, int bitsCount)
-        //{
-        //    byte[] ret = BitConverter.GetBytes(value);
-        //    if (ret.Length * 8 < bitsCount)
-        //    {
-        //        ret = new byte[ret.Length+()]
-        //    }
-        //}
-
         private Bitmap PutMsg(Bitmap ImgToMod, byte[] MsgToPut)
         {
             byte[] tempLen = BitConverter.GetBytes(MsgToPut.Length);
             byte[] msgLen = new byte[2];
-            msgLen[0] = tempLen[0];
-            msgLen[1] = tempLen[1];
+            msgLen[0] = tempLen[1];
+            msgLen[1] = tempLen[0];
             byte[] NewMsg = new byte[msgLen.Length + MsgToPut.Length];
             Buffer.BlockCopy(msgLen, 0, NewMsg, 0, msgLen.Length);
             Buffer.BlockCopy(MsgToPut, 0, NewMsg, msgLen.Length, MsgToPut.Length);
 
-            for (int i=0; i<ImgToMod.Width; ++i)
+            int x = 0, y = 0,
+                i;
+            for (i=0; i < NewMsg.Length; ++i)
             {
-                for (int j=0; j<ImgToMod.Height; ++j)
+                for (int j = 0; j < 4; ++j)
                 {
-                    Color CurPixel = ImgToMod.GetPixel(i, j);
-                    int R = CurPixel.R - CurPixel.R % 23,
-                        G = CurPixel.G - CurPixel.G % 23,
-                        B = CurPixel.B - CurPixel.B % 23;
-                    // ukryć wiadomość
-                    ImgToMod.SetPixel(i, j, Color.FromArgb(R, G, B));
+                    Color CurPixel = ImgToMod.GetPixel(x, y);
+                    byte R = CurPixel.R,
+                        G = CurPixel.G,
+                        B = CurPixel.B,
+                        a1 = GetBit(CurPixel.R, 0),
+                        a2 = GetBit(CurPixel.G, 0),
+                        a3 = GetBit(CurPixel.B, 0);
+
+                    byte M1 = GetBit(NewMsg[i], i*2),    //x1
+                        M2 = GetBit(NewMsg[i], i*2+1);  //x2
+
+                    if (M1 == (a1 ^ a3))
+                    {
+                        if (M2 != (a2 ^ a3)) G ^= 1;
+                    }
+                    else
+                    {
+
+                        if (M2 == (a2 ^ a3)) R ^= 1;
+                        else B ^= 1;
+                    }
+
+                    ImgToMod.SetPixel(x, y, Color.FromArgb(R, G, B));
+
+                    ++x;
+                    if (x >= ImgToMod.Width)
+                    {
+                        x = 0;
+                        ++y;
+                        if (y >= ImgToMod.Height)
+                        {
+                            return null;
+                        }
+                    }
                 }
+
+
             }
+
             return ImgToMod;
+        }
+
+        private byte GetBit(byte fromByte, int bitNum)
+        {
+            return Convert.ToByte((fromByte >> bitNum) & 1);
         }
 
         private void LoadImgBtn_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog op = new OpenFileDialog();
-            op.Title = "Select a picture";
-            op.Filter = "Bitmap graphics|*.bmp";
+            OpenFileDialog op = new OpenFileDialog
+            {
+                Title = "Select a picture",
+                Filter = "Bitmap graphics|*.bmp"
+            };
+
             if (op.ShowDialog() == true)
             {
                 LoadedImg.Source = new BitmapImage(new Uri(op.FileName));
                 LoadedImgPath.Content = op.FileName;
                 LoadedImgPath.ToolTip = op.FileName;
                 _imageLoaded = true;
+                UpdateStatus("Image loaded.");
+            }
+            else UpdateStatus("Image not loaded.");
+        }
+
+        private void ReadMsgBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Bitmap ImgToRead = new Bitmap(LoadedImgPath.Content.ToString());
+            //byte[] EncodedMsg = EncodeMsg(CustomMsg.Text);
+            byte[] msg = ReadMsg(ImgToRead);
+            msg = DecodeMsg(msg);
+            CustomMsg.Text = msg.ToString();
+            UpdateStatus("Message read and decoded.");
+        }
+
+        private byte[] ReadMsg(Bitmap imgToRead)
+        {
+            byte[] lenInBytes = ReadTwoBytes(imgToRead.GetPixel(0, 0));
+            int len = lenInBytes[0] * 256 + lenInBytes[1];
+            for (int i = 1; i <= len; ++i)
+            {
+
+            }
+            return imgToRead;
+        }
+
+        private byte[] ReadTwoBytes(Color pixel)
+        {
+            byte M1 = GetBit(pixel.R, 0),
+                M2 = GetBit(pixel.G, 0);
+            if (GetBit(pixel.B, 0) == 0)
+            {
+                return new byte[] { M1, M2 };
+            }
+            else //a3 == 1
+            {
+                return new byte[] { (M1^=1), (M2^=1) };
             }
         }
 
@@ -143,6 +220,12 @@ namespace wizut_steganografia
 
             bitmap.UnlockBits(bitmapData);
             return bitmapSource;
+        }
+
+        private void UpdateStatus(string status)
+        {
+            DateTime localDate = DateTime.Now;
+            StatusLabel.Content = localDate + " - " + status;
         }
     }
 }
