d34fc9d0d9a5228b070a362aa7b1aae09eb4a38a
Jacek Wolny <jacekwolny8@wp.pl>
2019-11-19 14:52:29 +0100

    lab4 - wykrywanie szumu - zrobione

---
+++ JPEG/JPEGAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace JPEG
+{
+    class JPEGAnalyzer
+    {
+        private List<Point> _points;
+        private Bitmap _modBitmap;
+        private LockBitmap _lockOrig;
+        private LockBitmap _lockMod;
+        public JPEGAnalyzer (string path)
+        {
+            _points = new List<Point>();
+            Bitmap _origBitmap = new Bitmap(path);
+            MemoryStream memoryStream = new MemoryStream();
+            _origBitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            _modBitmap = new Bitmap(memoryStream);
+            memoryStream.Dispose();
+            _lockOrig = new LockBitmap(_origBitmap);
+            _lockMod = new LockBitmap(_modBitmap);
+        }
+
+        public List<Point> GetModifiedPixelsList()
+        {
+            //for(int i=0; i < _origBitmap.Width; ++i)
+            //{
+            //    for (int j = 0; j < _origBitmap.Height; ++j)
+            //    {
+            //        if (_origBitmap.GetPixel(i, j) != _modBitmap.GetPixel(i, j)) _points.Add(new Point(i, j));
+            //        //if ((_origBitmap.GetPixel(i, j).R != _modBitmap.GetPixel(i, j).R) || (_origBitmap.GetPixel(i, j).G != _modBitmap.GetPixel(i, j).G) || (_origBitmap.GetPixel(i, j).B != _modBitmap.GetPixel(i, j).B)) _points.Add(new Point(i, j));
+            //    }
+            //}
+            _lockOrig.LockBits();
+            _lockMod.LockBits();
+            for (int y = 0; y < _lockOrig.Height; y++)
+            {
+                for (int x = 0; x < _lockOrig.Width; x++)
+                {
+                    if (_lockOrig.GetPixel(x, y) != _lockMod.GetPixel(x, y))
+                    {
+                        _points.Add(new Point(x, y));
+                        _lockMod.SetPixel(x, y, Color.Red);
+                    }
+                }
+            }
+            _lockMod.UnlockBits();
+            _lockOrig.UnlockBits();
+            return _points;
+        }
+
+        public int GetNumOfModifiedPixels()
+        {
+            return _points.Count;
+        }
+
+        public Bitmap MarkPixels()
+        {
+            return _modBitmap;
+        }
+    }
+}
---
+++ JPEG/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JPEG
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImgBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Title = "Select a picture",
+                Filter = "Bitmap graphics|*.bmp"
+            };
+
+            if (ofd.ShowDialog() == true)
+            {
+                LoadedImg.Source = new BitmapImage(new Uri(ofd.FileName));
+                LoadedImgPath.Content = ofd.FileName;
+                LoadedImgPath.ToolTip = ofd.FileName;
+                ModPixelCounter.Content = "0";
+                UpdateStatus("Image loaded.");
+            }
+            else UpdateStatus("Image not loaded.", true);
+        }
+
+        private void ImgPath_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("explorer.exe", Directory.GetParent(((System.Windows.Controls.Label)sender).Content.ToString()).FullName);
+                UpdateStatus("Dir opened.");
+            }
+            catch
+            {
+                UpdateStatus("Cannot open the dir.", true);
+            }
+        }
+
+        private void UpdateStatus(string status, bool isError = false)
+        {
+            DateTime localDate = DateTime.Now;
+            StatusLabel.Foreground = isError ? System.Windows.Media.Brushes.Red : System.Windows.Media.Brushes.Green;
+            StatusLabel.Content = localDate + " - " + status;
+            StatusLabel.ToolTip = status;
+        }
+
+        private System.Drawing.Bitmap BitmapSource2Bitmap(BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+            }
+            return bitmap;
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+        public ImageSource ImageSourceFromBitmap(System.Drawing.Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+        private void MarkChangesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LoadedImg.Source = MarkChanges(LoadedImgPath.Content.ToString());
+                UpdateStatus("Changes marked.");
+            }
+            catch (Exception err)
+            {
+                UpdateStatus(err.Message, true);
+            }
+        }
+
+        private ImageSource MarkChanges(string path)
+        {
+            JPEGAnalyzer detector = new JPEGAnalyzer(path);
+            List<System.Drawing.Point> pixels = detector.GetModifiedPixelsList();
+            if (pixels == null) throw new Exception("No pixels were changed.");
+            ModPixelCounter.Content = detector.GetNumOfModifiedPixels();
+            return ImageSourceFromBitmap(detector.MarkPixels());
+        }
+    }
+}
